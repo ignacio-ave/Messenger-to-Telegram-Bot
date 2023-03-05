c20600bbada2107200ab89a18b0693e8ec6d5012
Ignacio A <105741412+ignacio-ave@users.noreply.github.com>
2023-03-04 21:12:02 -0300

    Update bottelegram.py

--- bottelegram.py
+++ bottelegram.py
@@ -1,41 +1,47 @@
 import telegram
 from flask import Flask, request, abort
+import hmac
+from hashlib import sha1
 
-app = Flask(__name__)
-bot = telegram.Bot(token='tu_token_de_acceso_al_bot_de_telegram')
-chat_id = 'tu_chat_id'
+class BotTelegram:
+    def __init__(self, token, id_chat):
+        self.bot = telegram.Bot(token=token)
+        self.id_chat = id_chat
 
-@app.route('/telegram', methods=['POST'])
-def telegram_webhook():
-    if request.method == 'POST':
-        update = telegram.Update.de_json(request.get_json(force=True), bot)
-        handle_update(update)
-    return 'ok'
+    def enviar_mensaje(self, mensaje):
+        self.bot.send_message(chat_id=self.id_chat, text=mensaje)
 
-def handle_update(update):
-    text = update.message.text.lower()
-    if text == 'estoy interesado':
-        user_name = update.message.from_user.first_name
-        job = 'El trabajo en el que está interesado'
-        message = f'{user_name} está interesado en {job}.'
-        bot.send_message(chat_id=chat_id, text=message)
+class WebhookTelegram:
+    def __init__(self, bot, app, clave_secreta):
+        self.bot = bot
+        self.app = app
+        self.clave_secreta = clave_secreta
+
+        @app.route('/telegram', methods=['POST'])
+        def telegram_webhook():
+            if request.method == 'POST':
+                signature = request.headers.get('X-Hub-Signature')
+                if not self.verificar_firma(signature, request.data, self.clave_secreta):
+                    abort(400)
+                update = telegram.Update.de_json(request.get_json(force=True), bot.bot)
+                self.procesar_actualizacion(update)
+            return 'ok'
+
+    def procesar_actualizacion(self, actualizacion):
+        texto = actualizacion.message.text.lower()
+        if texto == 'estoy interesado':
+            nombre_usuario = actualizacion.message.from_user.first_name
+            trabajo = 'El trabajo en el que está interesado'
+            mensaje = f'{nombre_usuario} está interesado en {trabajo}.'
+            self.bot.enviar_mensaje(mensaje)
+
+    def verificar_firma(self, firma, datos, clave_secreta):
+        clave_bytes = bytes(clave_secreta, 'latin-1')
+        mac = hmac.new(clave_bytes, msg=datos, digestmod=sha1)
+        return hmac.compare_digest('sha1=' + mac.hexdigest(), firma)
 
 if __name__ == '__main__':
+    app = Flask(__name__)
+    bot = BotTelegram(token='tu_token_de_acceso_al_bot_de_telegram', id_chat='tu_id_de_chat')
+    webhook = WebhookTelegram(bot, app, 'tu_clave_secreta')
     app.run()
-'''
-
-## Ejemplo de uso
-
-```python 
-import telegram
-
-bot = telegram.Bot(token='tu_token_de_acceso_al_bot_de_telegram')
-chat_id = 'tu_chat_id'
-
-bot.send_message(chat_id=chat_id, text='Hola, soy un bot de Telegram')
-```
-
-## Referencias
-
-- [Telegram Bot API](https://core.telegram.org/bots/api)
-- [python-telegram-bot](https://github.com/python-telegram-bot/python-telegram-bot)
