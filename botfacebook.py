e1432aa8bb20bac4f8a6abeda078586eb9fa40e7
Ignacio A <105741412+ignacio-ave@users.noreply.github.com>
2023-03-04 21:11:34 -0300

    Update botfacebook.py

--- botfacebook.py
+++ botfacebook.py
@@ -1,5 +1,7 @@
 import requests
 import json
+import hmac
+import hashlib
 
 class Bot:
     def __init__(self, telegram_token):
@@ -20,13 +22,19 @@ class Bot:
         pass
 
 class FacebookBot:
-    def __init__(self, bot, fb_token):
+    def __init__(self, bot, fb_token, app_secret):
         self.bot = bot
         self.fb_token = fb_token
+        self.app_secret = app_secret
 
     def verify_fb_token(self, x_hub_signature, request_data, token):
-        # Implementar esta función según corresponda
-        pass
+        expected_signature = hmac.new(
+            bytes(self.app_secret, 'latin-1'),
+            msg=request_data,
+            digestmod=hashlib.sha1
+        ).hexdigest()
+
+        return hmac.compare_digest(x_hub_signature.split('=')[1], expected_signature)
 
     def handle_message(self, sender_id, message):
         if message.lower() == "ayuda":
@@ -43,24 +51,26 @@ class FacebookBot:
         elif message == "estoy interesado":
             # Obtener información del usuario y trabajo
             user_profile = self.bot.get_user_profile(sender_id)
-            first_name = user_profile["first_name"]
-            last_name = user_profile["last_name"]
-            job = "trabajo"
+            nombre = user_profile["first_name"]
+            apellido = user_profile["last_name"]
+            trabajo = "trabajo"
             # Enviar notificación a Telegram
-            message = f"{first_name} {last_name} está interesado en {job}"
+            mensaje = f"{nombre} {apellido} está interesado en {trabajo}"
             chat_id = "ID_DE_CHAT_DE_TELEGRAM"
-            self.bot.send_telegram_message(chat_id, message)
+            self.bot.send_telegram_message(chat_id, mensaje)
             # Responder al usuario
-            response = "Gracias por su interés. Nos pondremos en contacto con usted pronto."
-            self.bot.send_text_message(sender_id, response)
+            respuesta = "Gracias por su interés. Nos pondremos en contacto con usted pronto."
+            self.bot.send_text_message(sender_id, respuesta)
         else:
-            response = "Lo siento, no entendí su mensaje."
-            self.bot.send_text_message(sender_id, response)
+            respuesta = "Lo siento, no entendí su mensaje."
+            self.bot.send_text_message(sender_id, respuesta)
 
     def handle_webhook(self, request):
         if request.method == "POST":
             # Verificar la firma del webhook
-            if not self.verify_fb_token(request.headers.get("X-Hub-Signature"), request.data, self.fb_token):
+            x_hub_signature = request.headers.get("X-Hub-Signature")
+            request_data = request.data
+            if not self.verify_fb_token(x_hub_signature, request_data, self.fb_token):
                 abort(400)
 
             # Manejar el evento de webhook
@@ -76,4 +86,3 @@ class FacebookBot:
                                 self.handle_message(sender_id, text)
 
         return "OK"
-
